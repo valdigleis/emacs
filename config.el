28103364ab406f34c6b611045a7f3c48a1e1c3f4
Valdigleis (Dk4LL) <valdigleis@gmail.com>
2024-07-30 18:43:27 -0300

    🎨 Modificações de estrutura de arquivo config.org

--- config.el
+++ config.el
@@ -1,31 +1,41 @@
 ; Desabilitar tela de boas-vindas e ativas o scratch
-  (setq inhibit-startup-message t)
-  ; Desativar backups globalmente
-  (setq make-backup-files nil)
-  ; Habilita o salvamento da posição do cursor ao sair
-  (save-place-mode 1)
-  ; Habilita a funcionalidade de lembrar de arquivos recentes
-  (recentf-mode 1)
-  ; Habilita o salvamento do historico de comandos
-  (savehist-mode 1)
-  ; Habilita a atualização automática para os buffers dos arquivos modificados externamente
-  (setopt auto-revert-avoid-polling t)
-  ; Define o intervalo em segundos que o Emacs irá verificar se houve atualização externa dos arquivos
-  (setopt auto-revert-interval 60)
-  ; Habilita o Emacs para verificar informações de controle de versão automaticamente
-  (setopt auto-revert-check-vc-info t)
-  ; Habilita a movimentação entre janelas usando da combinação C-teclas direcionais
-  (windmove-default-keybindings 'control)
-  ; Desabilita a necessidade das sentenças terem que terminar com dois espaços
-  (setopt sentence-end-double-space nil)
-  ; Desabilita o sinal sonoro
-  (setq ring-bell-function 'ignore)
-  ; Desabilita o sinal visual
-  (when (display-graphic-p)
-    (setq visible-bell t))
-  ; desabilita o sinal visual (no modo gráfico)
-  (when (display-graphic-p)
-(setq visible-bell t))
+(setq inhibit-startup-message t)
+; Habilita o uso de espaços para a tabulação
+(setq-default indent-tabs-mode nil)
+; Habilita deletar texto selecionado ao digitar
+(delete-selection-mode t)
+; Desativar backups globalmente
+(setq make-backup-files nil)
+; Habilita o salvamento da posição do cursor ao sair
+(save-place-mode 1)
+; Habilita a funcionalidade de lembrar de arquivos recentes
+(recentf-mode 1)
+; Habilita o salvamento do historico de comandos
+(savehist-mode 1)
+; Habilita a atualização automática para os buffers dos arquivos modificados externamente
+(setopt auto-revert-avoid-polling t)
+; Define o intervalo em segundos que o Emacs irá verificar se houve atualização externa dos arquivos
+(setopt auto-revert-interval 60)
+; Habilita o Emacs para verificar informações de controle de versão automaticamente
+(setopt auto-revert-check-vc-info t)
+; Habilita a movimentação entre janelas usando da combinação C-teclas direcionais
+(windmove-default-keybindings 'control)
+; Desabilita a necessidade das sentenças terem que terminar com dois espaços
+(setopt sentence-end-double-space nil)
+; Desabilita o sinal sonoro
+(setq ring-bell-function 'ignore)
+; Desabilita o sinal visual
+(when (display-graphic-p)
+  (setq visible-bell t))
+; desabilita o sinal visual (no modo gráfico)
+(when (display-graphic-p)
+  (setq visible-bell t))
+; Habilita o uso de y e n para confirmações ou negações no Emacs
+(fset 'yes-or-no-p 'y-or-n-p)
+; Habilita o fechamento de pares
+(electric-pair-mode 1)
+; Habilita o uso do ESC para sair do minibuffer
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 ; Habilita a exibição do número da coluna
 (column-number-mode 1)
@@ -49,6 +59,8 @@
 ; Define a cor usada para o destaque da linha
 (set-face-background 'hl-line "#5e4a46")
 (set-face-foreground 'highlight nil)
+; Habilita a quebra visual de linha
+(global-visual-line-mode t)
 ; Habilita os indicadores visuais na margem esquerda da janela de edição
 (setopt indicate-buffer-boundaries 'left)
 ; Define a borda do Emacs em 10px (no modo gráfico)
@@ -61,11 +73,11 @@
 (setq display-line-numbers-type 'relative)
 ; Desabilitar números de linha para modos específicos
 (dolist (mode '(org-mode-hook
-  term-mode-hook
-  shell-mode-hook
-  eshell-mode-hook
-  help-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+                term-mode-hook
+                shell-mode-hook
+                eshell-mode-hook
+                help-mode-hook))
+        (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
