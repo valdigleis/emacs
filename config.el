7a02c7138c93bca236b38a13c4beacdb4ea4816f
Dk4LL <dk4ll@proton.me>
2024-08-10 17:50:26 -0300

    ✨ Inserção de texto de atenção!!!

--- config.el
+++ config.el
@@ -92,7 +92,6 @@
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
-
 (unless (package-installed-p 'use-package)
    (package-install 'use-package))
 (require 'use-package)
